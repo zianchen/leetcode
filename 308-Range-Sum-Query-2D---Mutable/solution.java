48721100122ec1613baa8bbf0b8bbfdae429ef8b
Zian Chen <ancenxixi@gmail.com>
2016-06-30 16:50:17 -0700

    [308. Range Sum Query 2D - Mutable][Accepted]committed by LeetCode Extension

---
+++ 308-Range-Sum-Query-2D---Mutable/solution.java
@@ -0,0 +1,58 @@
+public class NumMatrix {
+
+    private int[][] tree; // Binary indexed tree
+    private int[][] nums; // array that stores elements in matrix, we should avoid polluting the original input array(int[][] matrix)
+    private int m, n;
+    
+    public NumMatrix(int[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return;
+        }
+        m = matrix.length;
+        n = matrix[0].length;
+        tree = new int[m+1][n+1];
+        nums = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                update(i, j, matrix[i][j]);
+            }
+        }
+    }
+
+    public void update(int row, int col, int val) {
+        if (m == 0 || n == 0) {
+            return;
+        }
+        int delta = val - nums[row][col];
+        nums[row][col] = val;
+        for (int i = row + 1; i <= m; i += i & (-i)) {
+            for (int j = col + 1; j <= n; j += j & (-j)) {
+                tree[i][j] += delta;
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+        return sum(row2 + 1, col2 + 1) + sum(row1, col1) - sum(row1, col2 + 1) - sum(row2 + 1, col1);
+    }
+    
+    public int sum(int row, int col) {
+        int sum = 0;
+        for (int i = row; i > 0; i -= i & (-i)) {
+            for (int j = col; j > 0; j -= j & (-j)) {
+                sum += tree[i][j];
+            }
+        }
+        return sum;
+    }
+}
+
+
+// Your NumMatrix object will be instantiated and called as such:
+// NumMatrix numMatrix = new NumMatrix(matrix);
+// numMatrix.sumRegion(0, 1, 2, 3);
+// numMatrix.update(1, 1, 10);
+// numMatrix.sumRegion(1, 2, 3, 4);

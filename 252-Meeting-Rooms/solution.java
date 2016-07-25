c6c32492c9ee46bceebad00eb6e42d9483e6fa2e
Zian Chen <ancenxixi@gmail.com>
2016-07-25 14:23:23 -0700

    [252. Meeting Rooms][Accepted]committed by LeetCode Extension

--- 252-Meeting-Rooms/solution.java
+++ 252-Meeting-Rooms/solution.java
@@ -22,9 +22,13 @@ public class Solution {
         IntervalComparator intervalComparator = new IntervalComparator();
         Arrays.sort(intervals, intervalComparator);
         for (int i = 1; i < intervals.length; i++) {
-            int mStart = Math.max(intervals[i].start, intervals[i-1].start);
-            int mEnd = Math.min(intervals[i].end, intervals[i-1].end);
-            if (mStart < mEnd) {
+            // int mStart = Math.max(intervals[i].start, intervals[i-1].start);
+            // int mEnd = Math.min(intervals[i].end, intervals[i-1].end);
+            // if (mStart < mEnd) {
+            //     return false;
+            // }
+            // 因为这里是前后相邻的interval算overlapping，又加上已经按照start sort过了。所以前面的interval的start一定小于后面的interval的start。只有yihua哥考我的那个两个intervals list算交集的题目才需要这样去算overlapping
+            if (intervals[i].start < intervals[i-1].end) {
                 return false;
             }
         }

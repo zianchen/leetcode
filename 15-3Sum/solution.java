c64dcc0df59d738a7a3fd41d9c2ffd8a0968031a
Zian Chen <ancenxixi@gmail.com>
2016-08-15 21:45:23 -0700

    [15. 3Sum][Accepted]committed by LeetCode Extension

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -1,44 +1,21 @@
 public class Solution {
-    
-    private List<List<Integer>> res = new ArrayList<List<Integer>>();
     public List<List<Integer>> threeSum(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return res;
-        }
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i != 0 && nums[i] == nums[i-1]) {
-                continue;
+        List<List<Integer>> res = new LinkedList<>(); 
+        for (int i = 0; i < nums.length-2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i-1])) {
+                int lo = i+1, hi = nums.length-1, sum = 0 - nums[i];
+                while (lo < hi) {
+                    if (nums[lo] + nums[hi] == sum) {
+                        res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                        while (lo < hi && nums[lo] == nums[lo+1]) lo++;
+                        while (lo < hi && nums[hi] == nums[hi-1]) hi--;
+                        lo++; hi--;
+                    } else if (nums[lo] + nums[hi] < sum) lo++;
+                    else hi--;
+               }
             }
-            twoSum (i, nums);
         }
         return res;
-    }
-    
-    public void twoSum (int i, int[] nums) {
-        int left = i+1;
-        int right = nums.length - 1;
-        while (left < right) {
-            int sum = nums[i] + nums[left] + nums[right];
-            if (sum == 0) {
-                List<Integer> tmp = new ArrayList();
-                tmp.add(nums[i]);
-                tmp.add(nums[left]);
-                tmp.add(nums[right]);
-                res.add(tmp);
-                left++;
-                right--;
-                while (left < right && nums[left] == nums[left-1]) {
-                    left++;
-                }
-                while (left < right && nums[right] == nums[right+1]) {
-                    right--;
-                }
-            } else if (sum < 0) {
-                left++;
-            } else {
-                right--;
-            }
-        }
     }
 }

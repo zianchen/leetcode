97cb80fad76d4a6fff25574a88efcbeb95a16f28
Zian Chen <ancenxixi@gmail.com>
2016-08-22 15:45:33 -0700

    [127. Word Ladder][Accepted]committed by LeetCode Extension

--- 127-Word-Ladder/solution.java
+++ 127-Word-Ladder/solution.java
@@ -1,39 +1,38 @@
 public class Solution {
-    
-    private Queue<String> toVisit = new LinkedList();
     public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
-        // 把endword加进去的原因是这样最后一步做addNextWords的时候才能满足wordList.contains(nextStr)为true，因为最后一步会用log生成cog，如果cog不在wordList里面，那么nextStr就加不进去。
-        wordList.add(endWord);
-        int dist = 2;
-        addNextWords(beginWord, wordList);
-        while (!toVisit.isEmpty()) {
-            int num = toVisit.size();
-            for (int i = 0; i < num; i++) {
-                String candidate = toVisit.poll();
-                if (candidate.equals(endWord)) {
-                    return dist;
-                }
-                // wordList.remove(candidate);
-                addNextWords(candidate, wordList);
+        Set<String> beginSet = new HashSet();
+        Set<String> endSet = new HashSet<>();
+        beginSet.add(beginWord);
+        endSet.add(endWord);
+        int dist = 1;
+        while (!beginSet.isEmpty() && !endSet.isEmpty()) {
+            if (beginSet.size() > endSet.size()) {
+                Set<String> tmp = beginSet;
+                beginSet = endSet;
+                endSet = tmp;
             }
+            Set<String> nextStep = new HashSet();
+            for (String str : beginSet) {
+                char[] charStr = str.toCharArray();
+                for (int i = 0; i < charStr.length; i++) {
+                    char letter = charStr[i];
+                    for (int j = 0; j < 26; j++) {
+                        charStr[i] = (char) ('a' + j);
+                        String nextStr = String.valueOf(charStr);
+                        if (endSet.contains(nextStr)) {
+                            return dist + 1;
+                        }
+                        if (wordList.contains(nextStr)) {
+                            nextStep.add(nextStr);
+                            wordList.remove(nextStr);
+                        }
+                    }
+                    charStr[i] = letter;
+                }
+            }
+            beginSet = nextStep;
             dist++;
         }
         return 0;
-    }
-    
-    public void addNextWords(String candidate, Set<String> wordList) {
-        char[] str = candidate.toCharArray();
-        for (int i = 0; i < str.length; i++) {
-            char letter = str[i];
-            for (int j = 0; j < 26; j++) {
-                str[i] = (char)(j + 'a');
-                String nextStr = String.valueOf(str);
-                if (wordList.contains(nextStr)) {
-                    toVisit.add(nextStr);
-                    wordList.remove(nextStr);
-                }
-            }
-            str[i] = letter;
-        }
     }
 }

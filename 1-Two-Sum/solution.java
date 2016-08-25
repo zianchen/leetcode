c2c6a80b99b8bedf8c5ade25f6d2b2c705a6a0a6
Zian Chen <ancenxixi@gmail.com>
2016-08-25 13:08:34 -0700

    [1. Two Sum][Accepted]committed by LeetCode Extension

---
+++ 1-Two-Sum/solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    // 如果返回boolean，可以sort之后用two pointer，但是需要返回index的话，只能用hashmap
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}

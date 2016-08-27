ce015aebfb277a93df1af78b07c8183f21785da1
Zian Chen <ancenxixi@gmail.com>
2016-08-26 17:00:11 -0700

    [78. Subsets][Accepted]committed by LeetCode Extension

--- 78-Subsets/solution.java
+++ 78-Subsets/solution.java
@@ -5,20 +5,15 @@ public class Solution {
             return res;
         }
         Arrays.sort(nums);
-        List<Integer> tmp = new ArrayList<>();
-        res.add(tmp);
-        helper(tmp, 0, nums);
-        return res;
-    }
-
-    private void helper(List<Integer> tmp, int idx, int[] nums) {
-        for (int i = idx; i < nums.length; i++) {
-//            tmp.add(nums[idx]);  巨型bug,不是nums[idx],而是nums[i]
-            tmp.add(nums[i]);
-            res.add(new ArrayList<>(tmp));
-//            helper(tmp, idx +1, nums);
-            helper(tmp, i+1, nums);
-            tmp.remove(tmp.size()-1);
+        res.add(new ArrayList<>());
+        for (int i = 0; i < nums.length; i++) {
+            int size = res.size();
+            for (int j = 0; j < size; j++) {
+                List<Integer> newSet = new ArrayList<>(res.get(j));
+                newSet.add(nums[i]);
+                res.add(newSet);
+            }
         }
+        return res;
     }
 }
